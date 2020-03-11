a89b21872bf14a162fdb5c040378cc5c6f48841c
diegoarvz4 <darvz.gtz@gmail.com>
2020-03-11 15:05:36 -0600

    Production build added

---
+++ build/precache-manifest.19cb11331229f43a8ce75ca2e828dcb2.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8e5b0841f3f6ca655e3f4cf3c1f37db0",
+    "url": "/index.html"
+  },
+  {
+    "revision": "8eeb4dabbaede3900d51",
+    "url": "/static/css/main.a94e8589.chunk.css"
+  },
+  {
+    "revision": "90a978b3f7dc1031a4ee",
+    "url": "/static/js/2.d7566c61.chunk.js"
+  },
+  {
+    "revision": "f231859d6585c4cd5f80c344783ed269",
+    "url": "/static/js/2.d7566c61.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8eeb4dabbaede3900d51",
+    "url": "/static/js/main.c005c2f8.chunk.js"
+  },
+  {
+    "revision": "1f154c44703b8610609f",
+    "url": "/static/js/runtime-main.91c29192.js"
+  }
+]);
